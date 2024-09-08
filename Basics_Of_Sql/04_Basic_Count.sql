73d42a92fe5abad02e94d6eb433555b099bebcc5
Shaik Sonia <shaiksonia222@gmail.com>
2024-09-08 16:47:43 +0530

    Basic_Of_SQL

--- Basics_Of_Sql/04_Basic_Count.sql
+++ Basics_Of_Sql/04_Basic_Count.sql
@@ -7,3 +7,30 @@ select count(product_name) as products from testproducts
 select count(product_name)
 from testproducts
 where "Category_id" = 7
+
+<---it will give the total value of catergord id which sum of it------>
+select sum("Category_id") from testproducts
+
+<------it will return average value------>
+select avg("Category_id")::numeric(5,2) from testproducts
+
+<--LIKE B% will return the starting letter of that--------->
+select * from testproducts
+where "product_name" Like 'J%'
+
+<--LIKE B% will return the end letter of that--------->
+
+select * from testproducts
+where "product_name" Like '%a'
+
+<--LIKE B% will return the specific letter of that--------->
+
+select * from testproducts
+where "product_name" Like '%J%'
+
+<--LIKE B% will return the specific letter of that of the word--------->
+
+select * from testproducts
+where "product_name" Like 'J__n__'
+
+
